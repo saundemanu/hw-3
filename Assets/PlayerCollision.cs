05a73fb733c3afc746e04085750ee9a121f22467
andrewcopas <andacrews@gmail.com>
2020-02-28 11:59:05 -0800

    Andrews commit

---
+++ Assets/PlayerCollision.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour {
+   
+   public PlayerMovement movement;
+
+   void OnCollisionEnter(Collision collision){
+            if ( collision.collider.tag == "Wall") {
+                Debug.Log("BOOM");
+       }
+   }
+}
